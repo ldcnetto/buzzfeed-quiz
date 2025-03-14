767b34c3cc5d5712a44130a13d6a975e8d95e67a
ldcnetto <ldcn@cin.ufpe.br>
2025-03-14 17:07:28 -0300

    refactor: incluindo o import de HomeComponent e removendo coisas não usadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'buzzfeed-test';
 }
